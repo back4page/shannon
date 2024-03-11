2ed6a219ee0d8fe464bf180fe8658c103872b7b1
Yeasir Ahamed <yahamed007@gmail.com>
2024-03-11 20:31:45 +0600

    update content

--- src/app/page.js
+++ src/app/page.js
@@ -136,7 +136,7 @@ function Home() {
         <motion.section
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.2, delay:1 }}
+          transition={{ duration: 0.2, delay: 1 }}
           className="flex flex-col mt-[20px] mx-2 items-center px-16 py-11 text-base shadow-lg bg-gradient-to-r from-sky-100 to-orange-50 bg-opacity-10 rounded-[30px] xl:w-[60%] max-md:px-5"
         >
           <header className="text-3xl font-bold text-black text-opacity-80 text-center max-md:max-w-full">
@@ -200,28 +200,76 @@ function Home() {
 
         <div className="bg-[#6b727711] w-full py-16">
           <div className="max-w-2xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-5">
-            <div className="bg-[#ffffff63] rounded-md px-5 py-5">
+            <motion.div
+              initial={{
+                opacity: 0,
+                // if odd index card,slide from right instead of left
+                x: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0, // Slide in to its original position
+                transition: {
+                  duration: 0.5, // Animation duration
+                  delay: 0.2,
+                },
+              }}
+              viewport={{ once: true }}
+              className="bg-[#ffffff63] rounded-md px-5 py-5"
+            >
               <RiBarChartBoxLine className="text-4xl mb-2 text-amber-500" />
               <div className="flex justify-between gap-5 items-center">
                 <h1 className="text-4xl text-amber-500 font-semibold">30+</h1>
                 <h1 className="text-xl">Projects Done</h1>
               </div>
-            </div>
-            <div className="bg-[#ffffff63] rounded-md px-5 py-5">
+            </motion.div>
+            <motion.div
+              initial={{
+                opacity: 0,
+                // if odd index card,slide from right instead of left
+                x: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0, // Slide in to its original position
+                transition: {
+                  duration: 0.5, // Animation duration
+                  delay: 0.4,
+                },
+              }}
+              viewport={{ once: true }}
+              className="bg-[#ffffff63] rounded-md px-5 py-5"
+            >
               <IoHappyOutline className="text-4xl mb-2 text-cyan-500" />
               <div className="flex justify-between gap-5 items-center">
                 <h1 className="text-4xl text-cyan-500 font-semibold">8+</h1>
                 <h1 className="text-xl">Happy Client</h1>
               </div>
-            </div>
+            </motion.div>
 
-            <div className="bg-[#ffffff63] rounded-md px-5 py-5">
+            <motion.div
+              initial={{
+                opacity: 0,
+                // if odd index card,slide from right instead of left
+                x: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0, // Slide in to its original position
+                transition: {
+                  duration: 0.5, // Animation duration
+                  delay: 0.6,
+                },
+              }}
+              viewport={{ once: true }}
+              className="bg-[#ffffff63] rounded-md px-5 py-5"
+            >
               <LuBarChart3 className="text-4xl mb-2 text-teal-500" />
               <div className="flex justify-between gap-5 items-center">
                 <h1 className="text-4xl text-teal-500 font-semibold">20+</h1>
                 <h1 className="text-xl">Growing Team</h1>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
 
@@ -244,11 +292,11 @@ function Home() {
                   Education
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Our software solutions for schools streamline administrative
+                  tasks, enhance communication between teachers, students, and
+                  parents, and provide tools for personalized learning
+                  experiences, ensuring a conducive environment for academic
+                  success.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
@@ -257,11 +305,11 @@ function Home() {
                   Garments
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Tailored for the garment industry, our software optimizes
+                  production processes, manages inventory efficiently, tracks
+                  orders seamlessly, and provides insights to enhance
+                  decision-making, empowering garment manufacturers to stay
+                  competitive in a dynamic market.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
@@ -270,11 +318,11 @@ function Home() {
                   Healthcare
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Our healthcare software facilitates seamless patient
+                  management, electronic health record keeping, scheduling,
+                  billing, and reporting. With a focus on interoperability and
+                  data security, we empower healthcare providers to deliver
+                  high-quality care while improving operational efficiency.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
@@ -283,22 +331,23 @@ function Home() {
                   Fintech
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Our fintech solutions offer robust platforms for financial
+                  institutions, enabling secure transactions, risk management,
+                  compliance, and personalized customer experiences. With
+                  innovative tools for banking, payments, and wealth management,
+                  we drive digital transformation in the financial sector,
+                  enhancing agility and customer satisfaction.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
                 <TbMoneybag className="text-4xl" />
                 <h1 className="mt-7 text-xl font-bold text-stone-700">Ngo</h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Our software solutions for NGOs streamline donor management,
+                  fundraising efforts, volunteer coordination, and impact
+                  assessment. With tools for transparent reporting and resource
+                  allocation, we empower NGOs to maximize their social impact
+                  and achieve their mission effectively.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
@@ -307,11 +356,12 @@ function Home() {
                   Restaurant
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Our restaurant management software offers features for table
+                  reservation, order management, inventory tracking, staff
+                  scheduling, and customer engagement. With seamless integration
+                  with online ordering platforms and analytics for menu
+                  optimization, we help restaurants enhance operational
+                  efficiency and elevate guest experiences.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
@@ -320,11 +370,12 @@ function Home() {
                   Travel
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Tailored for the travel industry, our software facilitates
+                  booking management, itinerary planning, payment processing,
+                  and real-time communication with travelers. With integrations
+                  with global distribution systems and insights for trend
+                  analysis, we empower travel agencies and tour operators to
+                  provide memorable and seamless travel experiences.
                 </p>
               </div>
               <div className="flex flex-col grow px-4 py-11 w-full rounded-xl shadow-2xl bg-white bg-opacity-40 max-md:mt-6">
@@ -333,11 +384,12 @@ function Home() {
                   Retail
                 </h1>
                 <p className="mt-5 text-sm text-stone-500">
-                  When it comes to developing disruptive web products, Shannon
-                  takes an unconventional approach. Continue exploring new
-                  technologies, business models, and design approaches to create
-                  truly disruptive web products that can make a lasting impact
-                  on the industry
+                  Our retail software solutions enable inventory management,
+                  point-of-sale transactions, customer relationship management,
+                  and omnichannel sales strategies. With features for loyalty
+                  programs, promotions, and analytics-driven insights, we
+                  empower retailers to drive sales, improve customer
+                  satisfaction, and stay ahead in a competitive market.
                 </p>
               </div>
             </div>
